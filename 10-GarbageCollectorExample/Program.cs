b4aa513da2949251ae6d58be598ab918ea40fe03
EliasZare <1EliasZare1@gmail.com>
2024-11-05 12:08:28 +0330

    Add project files.

---
+++ 10-GarbageCollectorExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using _10_GarbageCollectorExample;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using Class1 class3 = new Class1();
+        Console.WriteLine($"Obj generation : {GC.GetGeneration(class3)}");
+
+        using Class1 class4 = new Class1();
+        Console.WriteLine($"Obj Total Memory : {GC.GetTotalMemory(false)}");
+        GC.Collect();
+        Console.WriteLine($"Obj Total Memory : {GC.GetTotalMemory(false)}");
+
+    }
+}
+
+namespace _10_GarbageCollectorExample
+{
+    public class Class1 : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose!");
+        }
+    }
+    public class Class2
+    {
+        public void Elias ()
+        {
+            using (Class1 class1 = new Class1())
+            {
+
+            }
+
+            using Class1 class2 = new Class1();
+        }
+    }
+}
